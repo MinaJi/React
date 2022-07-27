c4364e3c6624485fd436e4413f194c42310ffdcb
mina <minaaa0820@gmail.com>
2022-07-27 17:18:34 +0900

    no message

--- CH07_훅hook/ch07-app/src/hooks/1_useState/UseState01.jsx
+++ CH07_훅hook/ch07-app/src/hooks/1_useState/UseState01.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseState01 = () => {
+    const [time, setTime] = useState(0); // [state, setState]
+    
+    const handleClick = () => {
+
+        if (time >= 24) {
+            alert("증가못함")
+            setTime(0);
+        } else {
+            setTime(time + 1);
+        }
+    }
+
+
     return (
-        <div className='container mt-5' style={{color:'black', backgroundColor:'lightgray'}}> 
-            <input />
-        </div>
+        // <div className='container mt-5' style={{color:'black', backgroundColor:'lightgray'}}> 
+        //     <input />
+        // </div>
+        <>
+            <span>현재 시각: {time}시</span>
+            <button className='btn btn-dark' onClick={handleClick}>Update</button>
+        </>
     )
 }
 
